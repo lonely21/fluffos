d54c6fc213e761098720b961aa2757c4ed8fbe19
Yucong Sun <sunyucong@gmail.com>
2020-03-02 23:01:05 -0800

    efun: file_size fix inconsistent behavior under windows

--- testsuite/single/tests/efuns/file_size.c
+++ testsuite/single/tests/efuns/file_size.c
@@ -1,6 +1,16 @@
 void do_tests() {
     string str = "This is a test";
     rm("/test_file");
+
+    // no such file returns -1
+    ASSERT_EQ(-1, file_size("/test_file"));
+
     write_file("/test_file", str);
     ASSERT(file_size("test_file") == strlen(str));
+
+    ASSERT_EQ(strlen(read_file("/single/master.c")), file_size("/single/master.c"));
+
+    // Directory returns -2
+    ASSERT_EQ(-2, file_size("/single/tests"));
+    ASSERT_EQ(-2, file_size("/single/tests/"));
 }
